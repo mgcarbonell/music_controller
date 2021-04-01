e5df54df020abb6e8b402b41a359cc88043d0d3a
mgcarbonell <mgcarbonell@gmail.com>
2021-04-01 11:23:40 -0700

    Removes Secret Key

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p)!ty#8-j0q((p=^tw5pvg!yngc(_lcpt&*9p14t*5v(rgtcot'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
